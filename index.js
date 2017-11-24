b57234760dde57fcece2a04d0519d07da7387437
Pahan.Z <zagvzdin.pakhan@rambler.ru>
2017-11-25 01:03:13 +0300

    commit

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-	const output = document.getElementById('output'),
-		  message = document.getElementById('message'),
-		  submit = document.getElementById('form'),
-		  arr = [];
-	let state = localStorage.getItem('key');
-	state = state.split(',');
+const output = document.getElementById('output'),
+		message = document.getElementById('message'),
+		submit = document.getElementById('form'),
+		arr = [];
+let state = localStorage.getItem('key');
+state = state.split(',');
 
-	const addMessage = (mass, elem) => {
-		const newDiv = document.createElement('div');
-	 	newDiv.innerHTML = mass[elem];
-	 	output.appendChild(newDiv);
+const addMessage = (mass, elem) => {
+	const newDiv = document.createElement('div');
+	newDiv.innerHTML = mass[elem];
+	output.appendChild(newDiv);
+}
+
+
+
+if (state != null) {
+	for (var i = 0; i < state.length; i++) {			
+		addMessage(state, i)
 	}
-	
+}
 
 
-	if (state != null) {
-		for (var i = 0; i < state.length; i++) {			
-			addMessage(state, i)
-		}
-	}
-
-	
-	submit.addEventListener('submit', (event) => {
-		event.preventDefault();
-		arr.push(message.value);
+submit.addEventListener('submit', (event) => {
+	event.preventDefault();
+	arr.push(message.value);
+	addMessage(arr, arr.length-1);
+	message.value = '';
+	setTimeout(() => {
+		arr.push(`эмуляция ответа`);
 		addMessage(arr, arr.length-1);
-	 	message.value = '';
-	 	setTimeout(() => {
-	 		arr.push(`эмуляция ответа`);
-	 		addMessage(arr, arr.length-1);
-	 		localStorage.setItem('key', arr);
-	 	},1000);
-	 	localStorage.setItem('key', arr);
-	})
+		localStorage.setItem('key', arr);
+	},1000);
+	localStorage.setItem('key', arr);
+})
