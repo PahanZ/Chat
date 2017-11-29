1208b5913f2392d8e29bf1c7e670914fd3cf0854
PahanZ <zagvzdin.pakhan@rambler.ru>
2017-11-29 16:05:21 +0300

    localSt

--- index.js
+++ index.js
@@ -1,41 +1,50 @@
 const output = document.getElementById('output');
 const message = document.getElementById('message');
 const submit = document.getElementById('form');
+const usersMessage = [];
 const obj = {
   name: 'user',
-  message: [],
+  message: '',
+  date: new Date(),
+  showDate() {
+    return `${this.date.getHours()}:${this.date.getMinutes()}:${this.date.getSeconds()}`;
+  },
+};
+const serv = {
+  name: 'server',
+  message: '',
   date: new Date(),
   showDate() {
     return `${this.date.getHours()}:${this.date.getMinutes()}:${this.date.getSeconds()}`;
   },
 };
 
-const state = JSON.parse(localStorage.getItem('key'));
+const state = JSON.parse(localStorage.getItem('usersMessage'));
 
-
-const addMessage = (arr, elem) => {
+const addMessage = (sms) => {
   const newDiv = document.createElement('div');
-  newDiv.innerHTML = arr[elem];
+  newDiv.innerHTML = sms;
   output.appendChild(newDiv);
-  const str = JSON.stringify(obj);
-  localStorage.setItem('key', str);
-  console.log(obj.showDate());
+  const str = JSON.stringify(usersMessage);
+  localStorage.setItem('usersMessage', str);
 };
 
 
 if (state != null) {
-  Array.prototype.forEach.call(state.message, (item, i) => {
-    addMessage(state.message, i);
+  Array.prototype.forEach.call(state, (item) => {
+    addMessage(item.message);
   });
 }
 
 submit.addEventListener('submit', (event) => {
   event.preventDefault();
-  obj.message.push(message.value);
-  addMessage(obj.message, obj.message.length - 1);
+  obj.message = message.value;
+  addMessage(obj.message);
+  usersMessage.push(obj);
   message.value = '';
   setTimeout(() => {
-    obj.message.push('эмуляция ответа');
-    addMessage(obj.message, obj.message.length - 1);
+    serv.message = 'эмуляция ответа';
+    addMessage(serv.message);
+    usersMessage.push(serv);
   }, 1000);
 });
